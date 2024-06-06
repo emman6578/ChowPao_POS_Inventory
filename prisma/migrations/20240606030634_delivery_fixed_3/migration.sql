4d180615063eaf15e36fa8a3829c736d0be2b8d8
emman6578 <122762898+emman6578@users.noreply.github.com>
2024-06-06 15:54:21 +0800

    Delete prisma/migrations directory

--- prisma/migrations/20240606030634_delivery_fixed_3/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `delivery` ADD COLUMN `total_delivery_price` DOUBLE NULL,
-    ADD COLUMN `total_delivery_quantity` INTEGER NULL;
--- prisma/migrations/20240606051652_delivery_fixed_4/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `deliveryproducts` DROP FOREIGN KEY `DeliveryProducts_delivery_id_fkey`;
-
--- AddForeignKey
-ALTER TABLE `DeliveryProducts` ADD CONSTRAINT `DeliveryProducts_delivery_id_fkey` FOREIGN KEY (`delivery_id`) REFERENCES `Delivery`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
